41af55c2e176b4909802f4292644fa6732619ea2
Soumi2015 <debasish.bios@gmail.com>
2026-07-08 19:43:03 +0530

    Add files via upload
    Polygons:
    Outline:
    Set the screen for turtle graphics, including its size and title Set the screen color as per your choice Create an equilateral triangle using turtle Create a square using turtle

---
+++ Polygons.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("yellow")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.right(90)
+board.forward(80)
+
+board.right(90)
+board.forward(100)
+
+board.right(90)
+board.forward(80)
+
+turtle.done()
