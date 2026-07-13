b67f9ecaa04c5c7e84b4106af4189ac87592aca1
Soumi2015 <debasish.bios@gmail.com>
2026-07-13 19:58:16 +0530

    Add files via upload
    This project allows the user to enter any number, and it then replies whether it is a natural number or vice versa.

---
+++ Acp_natural_numbers_hw.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to check"))
+print("Number to be checked :", number)
+
+if number>0:
+    print("This number is a natural number")
+
+else:
+    print("This number is not a natural number")
